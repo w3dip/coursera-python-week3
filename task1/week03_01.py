50e6e6b28e31faa84babe13703608fc913f1fb0c
Павлов Иван Иванович <w3dipmail@gmail.com>
2021-02-25 23:14:26 +0300

    FileReader class

--- task1/week03_01.py
+++ task1/week03_01.py
@@ -2,3 +2,10 @@ class FileReader:
 
     def __init__(self, file_name):
         self.file_name = file_name
+
+    def read(self):
+        try:
+            file = open(self.file_name, "r")
+            return file.read()
+        except FileNotFoundError:
+            return ""
